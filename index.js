3f93e16b7dce9eddf40eae86c55dff29ce32c4b8
vishal <vishalbabar7351@gmail.com>
2024-07-22 23:55:50 +0530

    add index.html and variable in Js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 console.log("Hy Vishal I am Back With Js!");
+console.log("Hello World!");
+console.log(22);
+
+//variables In Js......... var, let, const
+// DataType varName = value;  strongly type lang
+//_js = loos
+
+var myName = "Vishal";
+myName = 33;
+myName = "Vishal";
+console.log(myName);
+
+//var ==> Global
+
+if(true){
+    var age = 44;
+}
+
+console.log(age);
+
+// let and const 
+
+if(true){
+  let ager = 22;
+  console.log(ager);
+}
+
+
+
+
+
+
+
+
