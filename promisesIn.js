a36a6b03bbace5bef6b1bcce1336d79efd0c1874
vishal <vishalbabar7351@gmail.com>
2024-07-27 20:31:29 +0530

    PromisesIn_JS

---
+++ promisesIn.js
@@ -0,0 +1,20 @@
+let a = 33;
+let b = 22;
+let result = a + b;
+
+//sync!   on the spot aveleble.
+//asyne      thst are return  the Promises. Becose that result tece time
+
+async function getData(){
+    let resultPromissFromServer = await fetch("https://github.com/BabarVishal/DBconectionInBackend");
+console.log(await resultPromissFromServer.json());
+}
+
+getData();
+console.log(result);
+
+
+fetch("https://github.com/BabarVishal/DBconectionInBackend")
+.then((data)=>{console.log(data)})   // This promise Will be resolve Properly 
+.catch((error)=>{console.log(error)})  // They Show me Error
+.finally();     // This promise Will be resolve Properly // or // They Show me Error
